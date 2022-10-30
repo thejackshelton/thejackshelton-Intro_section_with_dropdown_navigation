bb59a6b66143f84a8f1ecb542df82cd7ff3844e5
thejackshelton <jacksheltontx@outlook.com>
2022-10-30 13:26:59 -0500

    nav focus issue

--- public/app.js
+++ public/app.js
@@ -1,4 +1,8 @@
 // Menu
+const navButtons = document.querySelectorAll(
+  "#company-button",
+  "#features-button"
+);
 const openButton = document.querySelector("#open-button");
 const closeButton = document.querySelector("#close-button");
 const openMenu = document.querySelector("#open-menu");
@@ -6,11 +10,41 @@ const body = document.querySelector("body");
 
 //Menu Items
 const companyDiv = document.querySelector("#company-div");
-const companyLI = document.querySelector("#company-li");
+const companyButton = document.querySelector("#company-button");
 const featuresDiv = document.querySelector("#features-div");
-const featuresLI = document.querySelector("#features-li");
+const featuresButton = document.querySelector("#features-button");
 const companyDownArrow = document.querySelector("#company-down-arrow");
 const featuresDownArrow = document.querySelector("#features-down-arrow");
+
+for (let i = 0; i < navButtons.length; i++) {
+  navButtons[i].addEventListener("click", (e) => {
+    if (navButtons[i] === companyButton) {
+      companyDiv.classList.toggle("hidden");
+      companyDownArrow.classList.toggle("rotate-180");
+
+      // Setting aria-expanded for accessibility
+      companyButton.setAttribute(
+        "aria-expanded",
+        companyButton.getAttribute("aria-expanded") === "true"
+          ? "false"
+          : "true"
+      );
+    }
+
+    if (navButtons[i] === featuresButton) {
+      featuresDiv.classList.toggle("hidden");
+      featuresDownArrow.classList.toggle("rotate-180");
+
+      // Setting aria-expanded for accessibility
+      featuresButton.setAttribute(
+        "aria-expanded",
+        featuresButton.getAttribute("aria-expanded") === "true"
+          ? "false"
+          : "true"
+      );
+    }
+  });
+}
 
 openButton.addEventListener("click", () => {
   openMenu.classList.toggle("translate-x-full");
@@ -34,24 +68,24 @@ closeButton.addEventListener("click", () => {
   body.style.background = "none";
 });
 
-companyLI.addEventListener("click", () => {
-  companyDiv.classList.toggle("hidden");
-  companyDownArrow.classList.toggle("rotate-180");
+// companyButton.addEventListener("click", () => {
+//   companyDiv.classList.toggle("hidden");
+//   companyDownArrow.classList.toggle("rotate-180");
 
-  // Setting aria-expanded for accessibility
-  companyLI.setAttribute(
-    "aria-expanded",
-    companyLI.getAttribute("aria-expanded") === "true" ? "false" : "true"
-  );
-});
+//   // Setting aria-expanded for accessibility
+//   companyButton.setAttribute(
+//     "aria-expanded",
+//     companyButton.getAttribute("aria-expanded") === "true" ? "false" : "true"
+//   );
+// });
 
-featuresLI.addEventListener("click", () => {
-  featuresDiv.classList.toggle("hidden");
-  featuresDownArrow.classList.toggle("rotate-180");
+// featuresButton.addEventListener("click", () => {
+//   featuresDiv.classList.toggle("hidden");
+//   featuresDownArrow.classList.toggle("rotate-180");
 
-  // Setting aria-expanded for accessibility
-  featuresLI.setAttribute(
-    "aria-expanded",
-    featuresLI.getAttribute("aria-expanded") === "true" ? "false" : "true"
-  );
-});
+//   // Setting aria-expanded for accessibility
+//   featuresButton.setAttribute(
+//     "aria-expanded",
+//     featuresButton.getAttribute("aria-expanded") === "true" ? "false" : "true"
+//   );
+// });
